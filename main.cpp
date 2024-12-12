862c5120f947e2a2528904128f7c71bdcf28d227
Angela Dang <75496461+angeladangg@users.noreply.github.com>
2024-12-12 13:19:41 -0800

    Update main.cpp to accomodate to switch instead of button

--- main.cpp
+++ main.cpp
@@ -4,19 +4,17 @@
 #include <Arduino.h>
 #include <SparkFunLSM6DSO.h>
 
-#define BUTTON_PIN 17        // Pin for button
-#define STEP_THRESHOLD 1.2   // Adjust based on calibration
-#define STEP_DELAY 200       // Time delay for step detection in milliseconds
-#define SAMPLE_COUNT 50      // Number of samples to use for intensity calculation
-#define SAMPLE_DELAY_MS 20   // Delay between each sample for intensity calculation
+#define SWITCH_PIN 17      // Pin for switch
+#define STEP_THRESHOLD 1.2 // Adjust based on calibration
+#define STEP_DELAY 200     // Time delay for step detection in milliseconds
+#define SAMPLE_COUNT 50    // Number of samples to use for intensity calculation
+#define SAMPLE_DELAY_MS 20 // Delay between each sample for intensity calculation
 
+const char *ssid = "NeelPhone";
+const char *password = "test1111";
 
-const char* ssid = "DANG";
-const char* password = "appleman";
-
-
-const char* AWS_IP = "3.91.132.124"; // Server IP
-const char* serverPath = "/";
+const char *AWS_IP = "44.207.6.203"; // Server IP
+const char *serverPath = "/";
 const int serverPort = 5000;
 
 // Step counter variables
@@ -32,18 +30,21 @@ LSM6DSO myIMU;
 
 // Button state variables
 bool intensityMeasuring = false;
-//unsigned long lastButtonPress = 0;
+// unsigned long lastButtonPress = 0;
 unsigned long debounceDelay = 300; // debounce delay in milliseconds
-unsigned long prevButtonState = 0;
+// unsigned long prevButtonState = 0;
 // Function declarations
 float calculateRMS(float x[], float y[], float z[]);
 String getIntensityLevel(float intensity);
 
-
-
-void sendDataToAWS(int stepCount, float intensity) {
-  if (WiFi.status() == WL_CONNECTED) { // Check Wi-Fi connection
+void sendDataToAWS(int stepCount, float intensity)
+{
+  Serial.print("WiFi status: ");
+  Serial.println(WiFi.status());
+  if (WiFi.status() == WL_CONNECTED)
+  { // Check Wi-Fi connection
     HTTPClient http;
+    Serial.print("CONNECTED TO CLOUD");
 
     // Construct the URL
     String serverURL = "http://" + String(AWS_IP) + ":" + String(serverPort) + serverPath;
@@ -56,64 +57,91 @@ void sendDataToAWS(int stepCount, float intensity) {
     // Send HTTP POST request
     int httpResponseCode = http.POST(payload);
 
-    if (httpResponseCode > 0) {
+    if (httpResponseCode > 0)
+    {
       // Success
       String response = http.getString();
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
       Serial.print("Server response: ");
       Serial.println(response);
-    } else {
+    }
+    else
+    {
       // Failed to send data
       Serial.print("Error code: ");
       Serial.println(httpResponseCode);
     }
 
     http.end(); // End HTTP connection
-  } 
+  }
+  else
+  {
+    Serial.println("WiFi Not Connected.");
+  }
+  // Serial.print("CONNECTION FAILED!!!!!!!!!!!!!!!!!");
+  // if (WiFi.status() == WL_CONNECTED)
+  // {
+  //   Serial.println("WiFi Connected.");
+  // }
+  // else
+  // {
+  //   Serial.println("WiFi Not Connected.");
+  // }
 }
 
-
-void setup() {
+void setup()
+{
   // Serial setup
   Serial.begin(9600);
   Wire.begin(21, 22);
   WiFi.begin(ssid, password);
   // Initialize button
-  pinMode(BUTTON_PIN, INPUT_PULLUP); // Assuming the button is wired to GND
-  digitalWrite(BUTTON_PIN, LOW); // Ensure button is not activated initially
+  pinMode(SWITCH_PIN, INPUT); // Assuming the button is wired to GND
+  // digitalWrite(SWITCH_PIN, LOW);     // Ensure button is not activated initially
+
+  Serial.print("WiFi status: ");
+  Serial.println(WiFi.status());
 
   // Initialize the IMU
-  if (myIMU.begin()) {
+  if (myIMU.begin())
+  {
     Serial.println("IMU Ready.");
-  } else {
+  }
+  else
+  {
     Serial.println("IMU Initialization Failed.");
-    while (1);
+    while (1)
+      ;
   }
 
   myIMU.initialize(BASIC_SETTINGS);
 }
 
-void loop() {
+void loop()
+{
   // Read button state (with debounce)
-  int buttonState = digitalRead(BUTTON_PIN);
-  Serial.print("button state = ");
-  Serial.println(buttonState);
-  if (buttonState == LOW && prevButtonState == HIGH) {// if (low == pressed) and in time
+  int switchState = digitalRead(SWITCH_PIN);
+  // Serial.print("button state = ");
+  // Serial.println(buttonState);
+  if (switchState == HIGH)
+  { // if (low == pressed) and in time
     // lastButtonPress = millis();
-    //prevButtonState = buttonState;
+    // prevButtonState = buttonState;
 
-    intensityMeasuring = !intensityMeasuring; // Toggle intensity measuring
-
-    if (intensityMeasuring) {
-      Serial.println("Workout intensity measuring started.");
-    } else {
-      Serial.println("Workout intensity measuring stopped.");
-    }
-    // delay(500);
+    intensityMeasuring = true; // Toggle intensity measuring to True
+    // Serial.println("Workout intensity measuring started.");
   }
-   prevButtonState = buttonState;
-  // Read accelerometer data
+
+  else
+  {
+    // Serial.println("Workout intensity not being measured.");
+    // delay(500);
+    intensityMeasuring = false; //Toggle intensity measuring to False
+
+  }
+
+    // Read accelerometer data
   float accelX = myIMU.readFloatAccelX();
   float accelY = myIMU.readFloatAccelY();
   float accelZ = myIMU.readFloatAccelZ();
@@ -128,7 +156,8 @@ void loop() {
 
   // Step detection based on acceleration magnitude
   unsigned long currentTime = millis();
-  if ((currentTime - lastStepTime) > STEP_DELAY && accelMagnitude > STEP_THRESHOLD) {
+  if ((currentTime - lastStepTime) > STEP_DELAY && accelMagnitude > STEP_THRESHOLD)
+  {
     stepCount++;
     lastStepTime = currentTime;
 
@@ -136,12 +165,13 @@ void loop() {
     Serial.print("Step Detected! Total Steps: ");
     Serial.println(stepCount);
     sendDataToAWS(stepCount, 0); // Send step count; intensity = 0 for now
-
   }
 
   // Collect data for intensity calculation only if measuring intensity
-  if (intensityMeasuring) {
-    for (int i = 0; i < SAMPLE_COUNT; i++) {
+  if (intensityMeasuring)
+  {
+    for (int i = 0; i < SAMPLE_COUNT; i++)
+    {
       ax[i] = myIMU.readFloatAccelX();
       ay[i] = myIMU.readFloatAccelY();
       az[i] = myIMU.readFloatAccelZ();
@@ -172,28 +202,35 @@ void loop() {
     Serial.print(intensityLevel);
     Serial.println(")");
     sendDataToAWS(stepCount, intensity); // Send step count and calculated intensity
-
   }
 
   delay(100); // Small delay for stability
 }
 
 // Function to calculate RMS for a set of data (accelerometer or gyroscope)
-float calculateRMS(float x[], float y[], float z[]) {
+float calculateRMS(float x[], float y[], float z[])
+{
   float sumSquares = 0.0;
-  for (int i = 0; i < SAMPLE_COUNT; i++) {
+  for (int i = 0; i < SAMPLE_COUNT; i++)
+  {
     sumSquares += pow(x[i], 2) + pow(y[i], 2) + pow(z[i], 2);
   }
   return sqrt(sumSquares / SAMPLE_COUNT);
 }
 
 // Function to categorize intensity level
-String getIntensityLevel(float intensity) {
-  if (intensity < 4.0) {
+String getIntensityLevel(float intensity)
+{
+  if (intensity < 4.0)
+  {
     return "Low";
-  } else if (intensity > 5.0 && intensity < 30) {
+  }
+  else if (intensity > 5.0 && intensity < 30)
+  {
     return "Medium";
-  } else {
+  }
+  else
+  {
     return "High";
   }
 }
